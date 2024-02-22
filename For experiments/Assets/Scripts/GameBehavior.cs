f362c44121caa895ca262030cf8a4a2830c0efb6
NatalieBern <bernatalie09@gmail.com>
2024-02-23 01:21:14 +0700

    basic GUI added

--- For experiments/Assets/Scripts/GameBehavior.cs
+++ For experiments/Assets/Scripts/GameBehavior.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string labelText = "Collect all 4 items and win your freedom!"; 
+    public int maxItems = 4;
+
     private int _itemsCollected = 0;
-    // 1
     public int Items
-    {
-        // 2
-        get { return _itemsCollected; }
-        // 3
+    {   get { return _itemsCollected; }
         set
         {
             _itemsCollected = value;
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+        
+        if (_itemsCollected >= maxItems)
+        {
+                labelText = "You’ve found all the items!"; 
+        }
+        else
+            {
+                labelText = "Item found, only" +
+                    (maxItems - _itemsCollected) + "more to go!";
+            }
+
         }
     }
-    private int _playerHP = 10;
-    // 4
+    private int _playerHP = 3;
+ 
     public int HP
     {
         get { return _playerHP; }
@@ -28,4 +37,19 @@ public class GameBehavior : MonoBehaviour
             Debug.LogFormat("Lives: {0}", _playerHP);
         }
     }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25),
+            "Player Health:" + _playerHP);
+
+        GUI.Box(new Rect(20, 50, 150, 25),
+            "Items Collected: " + _itemsCollected);
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50,
+            300, 50), labelText);
+    }
+
 }
+
+
